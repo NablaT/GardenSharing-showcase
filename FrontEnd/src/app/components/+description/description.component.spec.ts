28b87670e1cad7f17015501617922a084f7c414e
NablaT <remi.pourtier@gmail.com>
2016-07-29 16:18:14 +0200

    Description component

---
+++ FrontEnd/src/app/components/+description/description.component.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * Test Component DescriptionDescription
+ */
+import {DescriptionComponent} from "./description.component.ts";
+import {Component} from "@angular/core";
+@Component({
+    selector: 'test-description',
+    template: '<sd-description></sd-description>',
+    directives: []
+})
+class TestDescriptionComponent {}
---
+++ FrontEnd/src/app/components/+description/description.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Component DescriptionDescriptionComponent
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'description',
+    moduleId: module.id,
+    templateUrl: './description.component.html',
+    styleUrls : ['./description.component.css']
+})
+export class DescriptionComponent { }
